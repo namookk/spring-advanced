1462f8217310f0264b0f6a1bf4deec78466e76f2
namookk <papakang22@naver.com>
2022-01-26 12:52:00 +0900

    feat : 전략 패턴 예제 추가

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,69 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.template.code.SubClassLogic1;
+import hello.advanced.trace.template.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void strategyV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1(){
+        long startTime = System.currentTimeMillis();
+        //비지니스 로직 실행
+        log.info("비지니스 로직1 실행");
+        //비지니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+    private void logic2(){
+        long startTime = System.currentTimeMillis();
+        //비지니스 로직 실행
+        log.info("비지니스 로직2 실행");
+        //비지니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    /**
+     * 전략 패턴 적용
+     */
+    @Test
+    void strategyV1(){
+        ContextV1 context1 = new ContextV1(new StrategyLogic1());
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new StrategyLogic2());
+        context2.execute();
+    }
+
+    @Test
+    void strategyV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직1 실행!");
+            }
+        };
+        template1.execute();
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비지니스 로직2 실행!");
+            }
+        };
+        template2.execute();
+    }
+}
